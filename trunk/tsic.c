2d59ed0de7f6560fc911e6866b36dc472188490c
jam3sward@googlemail.com <jam3sward@googlemail.com@4d22e98f-478d-6b8a-cebc-028eb2199eb5>
2014-03-28 21:26:36 +0000

    Modified to read the entire packet (two bytes) in one attempt. Also tries to detect when sampling starts mid packet.

--- trunk/tsic.c
+++ trunk/tsic.c
@@ -38,9 +38,9 @@ static int parity8( int value )
 
 /*---------------------------------------------------------------------------*/
 
-/* Reads a 10 bit packet: 1 start bit, 8 data bits and 1 parity bit.
- * Only 9 bits are returned, consisting of the data bits and even parity bit
- * in bit 0 (the start bit is omitted).
+/* Read two packets from the device, strips the start bit off each and returns
+ * an 18 bit word consisting of two 9 bit values (data byte and even parity
+ * bit in the least significant bit)
  */
 int readPacket( void )
 {
@@ -50,18 +50,43 @@ int readPacket( void )
 	int high  = 0;  /* time spent high within one frame (bit) */
 	int half  = 0;  /* time for half one frame */
 	int frame = 0;  /* time for one frame */
-	
-	/* wait for the line to go low */
-	while ( gpio_get_value(tempGPIO) );
-	
-	/* time the low portion of the start bit, which is half the duty cycle */
-	while ( !gpio_get_value(tempGPIO) ) ++half;
-	
-	/* duration of a complete frame (usually around 125us) */
-	frame = 2*half;
+	int retry = 0;  /* count number of retries */
 
-    /* read the remaining 9 bits: data and parity */
-	for (i=0; i<9; ++i) {
+    /* There's a transmission lasting about 2.6ms every 100ms, and we could
+     * be anywhere in that cycle when we enter this function. There's a
+     * small chance (about 3%) that we could be inside a packet already.
+     * We try to detect the situation and retry if needed.
+     */
+    for (;;) {
+	    /* wait for the line to go low */
+	    while ( gpio_get_value(tempGPIO) ) ++high;
+	
+	    /* time the low portion of the start bit, which is half the duty cycle */
+	    while ( !gpio_get_value(tempGPIO) ) ++half;
+	    	
+	    /* duration of a complete frame (usually around 125us) */
+	    frame = 2*half;
+	
+	    /* if the line was high for less than two frames, then it looks as if
+	     * we started sampling part way through a packet (in fact, this very
+	     * rarely happens in practice)
+	     */
+	    if ( high < 2*frame ) {
+	        /* have we exceeded the maximum number of retries? */
+	        if ( ++retry > 1 ) return -1;
+	        
+	        /* updates are every 100ms and last around 2.6ms, we want to delay
+	         * until just before the next update. this could sleep for longer.
+	         */
+	        msleep( 95 );
+	    } else {
+	        /* this looks like the start of a good packet */
+	        break;
+	    }
+    }
+
+    /* read the remaining 19 bits */
+	for (i=0; i<19; ++i) {
 	    low  = 0;
 	    high = 0;
 	    
@@ -72,14 +97,25 @@ int readPacket( void )
 		while ( !gpio_get_value(tempGPIO) ) ++low;
 		
 		/* if the line is low for less than half the duty cycle, this must
-		 * be a high bit, which we store
-		 */
-		word <<= 1;		
-		if ( low < half ) word |= 1;
+		 * be a high bit, which we store. because we only examine the
+		 * low pulses, this will ignore the stop bit between the two packets
+		 */	
+		if ( low < half ) word |= 1<<(18-i);
 		
-		/* if the line is high for more than one frame, something is wrong */
-		if ( high > frame ) return -1;
+		/* if the line is high for more than 2 frames, something is wrong
+		 * note: it could legitimately be high for up to 1.75 frames if the
+	     * first packet ends with high parity (75% duty) followed by the 100%
+	     * duty stop bit between the two packets
+	     */
+		if ( high > 2*frame ) return -1;
 	}
+	
+	/* contains: <data><parity><start><data><parity> */
+	
+	/* strip out start bit in middle */
+	word = (word & 0x1FF) | ((word >> 1) & (0x1FF << 9));
+
+    /* now contains: <data><parity><data><parity> */
 
     /* return the result */
 	return word;
@@ -108,15 +144,15 @@ int readTemperature( void )
 	const int minTemp = -50 * scale;
 	const int maxTemp = 150 * scale;
 
-	/* read two packets: this is time critical */
-	packet0 = readPacket();
-	if ( packet0 >= 0 )
-	    packet1 = readPacket();
+	/* read packets from the sensor: this is time critical */
+	raw = readPacket();
+	/*printk( KERN_ALERT "tsic: %X\n", raw );*/
 	
-	if ( (packet0 < 0) || (packet1 < 0) ) {
-	    /*printk( KERN_ALERT "tsic: protocol error\n" );*/
-	    return INVALID_TEMP;
-	}
+	if ( raw < 0 ) return INVALID_TEMP;
+	
+	/* separate into two 9 bit words */
+	packet0 = (raw >> 9) & 0x1FF;	
+	packet1 = raw & 0x1FF;
 	
 	/* strip off the parity bit */
 	parity0 = packet0 & 1;
@@ -170,11 +206,15 @@ static ssize_t temp_show(
     /* read the temperature sensor */
     int temperature = readTemperature();
     
+    /* if that failed, have one more attempt */
+    if ( temperature == INVALID_TEMP )
+        temperature = readTemperature();
+    
     /* output the temperature if valid */
     if ( temperature != INVALID_TEMP )
         return sprintf( buffer, "%d\n", temperature );
     else
-        return 0;
+        return sprintf( buffer, "error\n" );
 }
 
 /*---------------------------------------------------------------------------*/
